3e195a4796c0350fd79f1ac3669597da68e8104b
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-09-09 11:20:34 +0300

    _PagingPartial and PagingViewModel

---
+++ Web/Restarauntly.Web.ViewModels/PagingViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Restarauntly.Web.ViewModels
+{
+    using System;
+
+    public class PagingViewModel
+    {
+        public int PageNumber { get; set; }
+
+        public bool HasPreviousPage => this.PageNumber > 1;
+
+        public bool HasNextPage => this.PageNumber < this.PagesCount;
+
+        public int PreviousPageNumber => this.PageNumber - 1;
+
+        public int NextPageNumber => this.PageNumber + 1;
+
+        public int PagesCount => (int)Math.Ceiling((double)this.RecipesCount / this.ItemsPerPage);
+
+        public int RecipesCount { get; set; }
+
+        public int ItemsPerPage { get; set; }
+    }
+}
