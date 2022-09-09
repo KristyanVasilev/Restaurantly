b9a8f58219c6a36ac6377704cc90ff265499d434
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-09-09 13:36:58 +0300

    ICategoriesService

---
+++ Services/Restarauntly.Services.Data/CategoriesService.cs
@@ -0,0 +1,30 @@
+﻿namespace Restarauntly.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Restarauntly.Data.Common.Repositories;
+    using Restarauntly.Data.Models;
+
+    public class CategoriesService : ICategoriesService
+    {
+        private readonly IDeletableEntityRepository<Category> categoryRepository;
+
+        public CategoriesService(IDeletableEntityRepository<Category> categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> GetCategories()
+        {
+            return this.categoryRepository.AllAsNoTracking().Select(x => new
+            {
+                x.Id,
+                x.Name,
+            })
+            .OrderBy(x => x.Name)
+            .ToList()
+            .Select(x => new KeyValuePair<string, string>(x.Id.ToString(), x.Name));
+        }
+    }
+}
---
+++ Services/Restarauntly.Services.Data/ICategoriesService.cs
@@ -0,0 +1,9 @@
+﻿namespace Restarauntly.Services.Data
+{
+    using System.Collections.Generic;
+
+    public interface ICategoriesService
+    {
+        IEnumerable<KeyValuePair<string, string>> GetCategories();
+    }
+}
