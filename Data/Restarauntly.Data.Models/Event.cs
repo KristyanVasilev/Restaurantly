06aa78acd01f0a679cd3a15c86507a276118d9e0
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-09-12 13:47:03 +0300

    Added Event data model

---
+++ Data/Restarauntly.Data.Models/Event.cs
@@ -0,0 +1,24 @@
+﻿namespace Restarauntly.Data.Models
+{
+    using System.Collections.Generic;
+
+    using Restarauntly.Data.Common.Models;
+
+    public class Event : BaseDeletableModel<int>
+    {
+        public Event()
+        {
+            this.Images = new HashSet<Image>();
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; }
+    }
+}
