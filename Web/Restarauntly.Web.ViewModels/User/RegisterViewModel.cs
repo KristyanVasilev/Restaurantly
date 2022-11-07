2418ba824fd6fd969e61f5e5ef5273acd051d740
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-07 15:40:26 +0200

    Added UserController

---
+++ Web/Restarauntly.Web.ViewModels/User/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Restarauntly.Web.ViewModels.User
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(60, MinimumLength = 10)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
---
+++ Web/Restarauntly.Web/Controllers/UserController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Restarauntly.Data.Models;
+using Restarauntly.Web.ViewModels.User;
+using System.Threading.Tasks;
+
+namespace Restarauntly.Web.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(
+            UserManager<ApplicationUser> _userManager,
+            SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Email,
+                UserName = model.Name
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+            return View(model);
+        }
+
+        //[HttpGet]
+        //[AllowAnonymous]
+        //public IActionResult Login()
+        //{
+        //    if (User?.Identity?.IsAuthenticated ?? false)
+        //    {
+        //        return RedirectToAction("All", "Books");
+        //    }
+
+        //    var model = new LoginViewModel();
+
+        //    return View(model);
+        //}
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> Login(LoginViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
+
+        //    var user = await userManager.FindByNameAsync(model.UserName);
+
+        //    if (user != null)
+        //    {
+        //        var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToAction("All", "Books");
+        //        }
+        //    }
+
+        //    ModelState.AddModelError("", "Invalid login");
+
+        //    return View(model);
+        //}
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
